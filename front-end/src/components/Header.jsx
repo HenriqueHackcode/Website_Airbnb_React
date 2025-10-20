0e77a5146b211c5afa84129591c4a306ada75ca7
Henrique Hackbart Neto <henriquehackcode@gmail.com>
2025-10-20 18:47:16 -0300

    Header

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import logo from "../assets/airbnb.svg";
+
+const Header = () => {
+  return (
+    <header className="shadow-md">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-8 py-4">
+        <div className="flex items-center">
+          <img src={logo} alt="Logo da Airbnb" className="h-14 w-14" />
+        </div>
+        <div className="flex items-center rounded-full border border-gray-300 py-2 pr-4 pl-8 shadow-md">
+          <p className="border-r border-r-gray-300 pr-4">Qualquer lugar</p>
+          <p className="border-r border-r-gray-300 px-4">Qualquer semana</p>
+          <p className="px-4">Hóspedes</p>
+
+          <div className="bg-primary-300 rounded-full text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2 rounded-full border border-gray-300 py-2 pr-4 pl-8 shadow-md">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 text-gray-600"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-8 text-gray-600"
+          >
+            <path
+              fillRule="evenodd"
+              d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <p className="px-4">Henrique XYZ</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
